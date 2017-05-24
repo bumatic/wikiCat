d02005dfc347d1a441de25aafa707a71aade98d5
Unknown <marcus.burkhardt@gmail.com>
2017-05-24 15:04:51 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ wikiCatTest.py
@@ -0,0 +1,36 @@
+from wikiCat.wikiproject import Project
+import pprint
+from wikiCat.processors.parsed_data_error_check import ParsedDataErrorCheck
+from wikiCat.processors.graph_data_gererator import GraphDataGenerator
+
+
+pp = pprint.PrettyPrinter(indent=4)
+
+mp = Project('project')
+mp.load_project()
+
+pp.pprint(mp.pinfo)
+
+#ParsedDataErrorCheck(mp, 'cat_data').missing_info_source_ids()
+#ParsedDataErrorCheck(mp, 'cat_data').find_unresolvable_target_titles()
+
+GraphDataGenerator(mp, 'cat_data').generate_graph_data()
+
+pp.pprint(mp.pinfo)
+'''
+# CODE FOR PROJECT CREATION:
+mp = Project('project')
+mp.create_project(title='MyWikiCatDevelopmentProject', description='This is my test project for WikiCatDevelopment')
+'''
+
+'''
+# CODE FOR LOADING PROJECT:
+mp = Project('project')
+mp.load_project()
+'''
+
+'''
+# CODE FOR ADDING PARSED DATA TO PROJECT:
+mp.add_data('parsed', page_info=['enwiki-20161101_page_info.csv'], revision_info=['enwiki-20161101_revisions.csv'], cat_data=['enwiki-20161101_cats.csv'])
+'''
+
