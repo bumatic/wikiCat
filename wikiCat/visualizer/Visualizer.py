183b6f9c6cd554e60165bd732db6a5a7cd909a87
Unknown <marcus.burkhardt@gmail.com>
2017-07-04 15:21:09 +0200

    Visualizer Test

--- wikiCat/visualizer/Visualizer.py
+++ wikiCat/visualizer/Visualizer.py
@@ -59,7 +59,7 @@ class Visualizer(GtGraphProcessor):
     def create_snapshot_view(self, path, file, stype):
         prop_map = stype + '_' + str(file[:-6])
         if prop_map in self.gt.edge_properties.keys():
-            print('PROP MAP EXISTS')
+            #print('PROP MAP EXISTS')
             prop_map = self.gt.edge_properties[prop_map]
         else:
             self.gt.list_properties()
@@ -309,7 +309,8 @@ class SFDP(Visualizer):
             out = os.path.join(self.results_path, 'sfpd_'+outfile+'.'+outtype)
             os.makedirs(os.path.dirname(out), exist_ok=True)
             pos = sfdp_layout(g)
-            graph_draw(g, pos, vprops=self.drawing_props['vprops'], eprops=self.drawing_props['eprops'], vertex_text_position=-2, output_size=(self.output_dimension, self.output_dimension), output=out)
+            graph_draw(g, pos, vprops=self.drawing_props['vprops'], eprops=self.drawing_props['eprops'],
+                       vertex_text_position=-2, output_size=(self.output_dimension, self.output_dimension), output=out)
         except Exception as e:
             print(e)
 
@@ -356,26 +357,34 @@ class RTL(Visualizer):
         Visualizer.__init__(self, graph)
 
     def visualize(self, graph_view, seed, outfile, outtype, vsize=None, vlabel=None, color_by_type=True, esize=None):
-        print('Create Viz')
+        #print('Create Viz')
         g = graph_view
         try:
             self.process_drawing_properties(g, vsize=vsize, vlabel=vlabel, color_by_type=color_by_type, esize=esize)
             out = os.path.join(self.results_path, 'RTL_'+outfile+'.'+outtype)
             os.makedirs(os.path.dirname(out), exist_ok=True)
-            pos = radial_tree_layout(g, seed)
-            graph_draw(g, pos, vprops=self.drawing_props['vprops'], eprops=self.drawing_props['eprops'], vertex_text_position=-2, output_size=(self.output_dimension, self.output_dimension), output=out)
+            pos = radial_tree_layout(g, seed[0])
+            graph_draw(g, pos, vprops=self.drawing_props['vprops'], eprops=self.drawing_props['eprops'],
+                       vertex_text_position=-2, output_size=(self.output_dimension, self.output_dimension), output=out)
         except Exception as e:
             print(e)
 
     def snapshots(self, stype, seed=None, outtype='png', vsize=None, vlabel=None, color_by_type=True, esize=None):
         self.load()
         print('(Sub) Graph loaded')
+        seed = graph_tool.util.find_vertex(self.gt, self.gt.vp.id, str(seed))
         self.results_path = os.path.join(self.results_path, stype)
         snapshot_files = self.data[self.working_graph][stype]['files']
         snapshot_path = os.path.join(self.graph.data[self.working_graph]['location'], stype)
-        graph_views = self.create_snapshot_views(snapshot_path, snapshot_files)
-        for key in graph_views.keys():
-            self.visualize(graph_views[key], seed, key, outtype, vsize=vsize, vlabel=vlabel, color_by_type=color_by_type, esize=esize)
+
+        for file in snapshot_files:
+            graph_view = self.create_snapshot_view(snapshot_path, file, stype)
+            self.visualize(graph_view, seed, file[:-4], outtype, vsize=vsize, vlabel=vlabel, color_by_type=color_by_type,
+                           esize=esize)
+
+        #graph_views = self.create_snapshot_view(snapshot_path, snapshot_files, stype)
+        #for key in graph_views.keys():
+        #    self.visualize(graph_views[key], seed, key, outtype, vsize=vsize, vlabel=vlabel, color_by_type=color_by_type, esize=esize)
 
 
 class DOT(GraphVizVisualizer):
--- wikiCatTest.py
+++ wikiCatTest.py
@@ -15,281 +15,86 @@ from wikiCat.visualizer.Visualizer import *
 
 mp = Project('project')
 mp.load_project()
-
 g = mp.gt_graph_objs['fixed_none__errors_removed']
-g.set_working_graph('main')
-#SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-#SFDP(g).snapshots('snapshot_year', vsize='cscore')
-#ARF(g).snapshots('snapshot_year', vsize='cscore') #, vlabel='title'
 
 
-'''
+
 g.set_working_graph('main')
-SubGraph(g).create(title='ethereum_article_super_3_gt_new', seed=[41754003], cats=True,
-                   subcats=0, supercats=3)
-g.set_working_graph('ethereum_article_super_3_gt_new')
+SubGraph(g).create(title='Geosocial_networking_3_sub_1_super_gt', seed=[26593966], cats=True,
+                   subcats=3, supercats=1)
+
+g.set_working_graph('Geosocial_networking_3_sub_1_super_gt')
 SubGraphProcessor(g).create_gt_subgraph()
 Snapshots(g).create('snapshot_year')
 SubGraphProcessor(g).internalize_snapshots('snapshot_year')
-'''
-g.set_working_graph('ethereum_article_super_3_gt_new')
-#g.set_working_graph('bitcoin_subcats_3_gt')
-#g.set_working_graph('main')
-#SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-DOT(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-#ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-
-'''
-
-
-g.set_working_graph('bitcoin_supercats_3_gt')
+#RTL(g).snapshots('snapshot_year', seed=22313095, vsize='cscore', vlabel='title', esize='cscore')
 SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-FR(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
+ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
 
 
-g.set_working_graph('bitcoin_supercats_4_gt')
-SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-FR(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
 
 
-g.set_working_graph('bitcoin_supercats_2_gt')
-SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-FR(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-'''
 
-#g.set_working_graph('bitcoin_subcats_2_gt')
-#SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-#ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-#FR(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
 
 '''
-g.set_working_graph('bitcoin_subcats_3_gt')
-SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-FR(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-'''
-'''
-g.set_working_graph('bitcoin_supercats_5_gt')
-SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-FR(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-
-g.set_working_graph('bitcoin_supercats_10_gt')
-SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-FR(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-'''
-
-'''
-g.set_working_graph('big_data_subcats')
-#RTL(g).snapshots('snapshot_year', seed=40423498, vsize='cscore', vlabel='title', esize='cscore')
-SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-FR(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-
-
 g.set_working_graph('main')
-SubGraph(g).create(title='bitcoin_gt', seed=[39563179], cats=True,
-                   subcats=5, supercats=5)
-g.set_working_graph('bitcoin_gt')
+SubGraph(g).create(title='Digital_media_5sub_gt', seed=[1017992], cats=True,
+                   subcats=5, supercats=0)
+g.set_working_graph('Digital_media_5sub_gt')
 SubGraphProcessor(g).create_gt_subgraph()
 Snapshots(g).create('snapshot_year')
 SubGraphProcessor(g).internalize_snapshots('snapshot_year')
-'''
-
-
-
-
-
-'''
-#SubGraph(g).create(title='bitcoin_subcats_gt', seed=[39563179], cats=True,
-#                   subcats=5, supercats=0)
-#g.set_working_graph('bitcoin_subcats_gt')
-
-
-
-#SubGraphProcessor(g).create_gt_subgraph()
-
-
-#Snapshots(g).create('snapshot_year')
-#SubGraphProcessor(g).internalize_snapshots('snapshot_year')
-
-#g.set_working_graph('main')
-#SubGraph(g).create('bitcoin_supercats_gt', 'snapshot_year',  seed=[39563179], cats=True,
-#                   subcats=0, supercats=5)
-
-
-
-#ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-
-#Snapshots(g).create('snapshot_month', slice='month')
-
-#sgp = SubGraphProcessor(g)
-#sgp.create_gt_subgraph()
-#sgp.internalize_snapshots('snapshot_year')
-
-
-
-
-g.set_working_graph('big_data_subcats')
-#RTL(g).snapshots('snapshot_year', seed=40423498, vsize='cscore', vlabel='title', esize='cscore')
+RTL(g).snapshots('snapshot_year', seed=39563179, vsize='cscore', vlabel='title', esize='cscore')
 SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-FR(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-'''
-
-#g.set_working_graph('bitcoin')
-#SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-#ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-#FR(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-
-'''ABOVE SFDP PRDUCES AND ERROR - CHECK
-
-nitialized graph visualizer
-(Sub) Graph loaded
-create Viz
-214
-<PropertyMap object with key type 'Vertex' and value type 'double', for Graph 0x1268a8a20, at 0x103203c50>
-create Viz
-370
-<PropertyMap object with key type 'Vertex' and value type 'double', for Graph 0x1268a86d8, at 0x103164240>
-Traceback (most recent call last):
-  File "wikiCatTest.py", line 26, in <module>
-    SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-  File "/Users/ga58sis/Desktop/wikiCat/wikiCat/visualizer/Visualizer.py", line 46, in snapshots
-    self.visualize(graph_views[key], key, outtype, vsize=vsize, vlabel=vlabel, color_by_type=color_by_type, esize=esize)
-  File "/Users/ga58sis/Desktop/wikiCat/wikiCat/visualizer/Visualizer.py", line 186, in visualize
-    graph_draw(g, pos, vprops=self.drawing_props['vprops'], eprops=self.drawing_props['eprops'], vertex_text_position=-2, output_size=self.output_size, output=out)
-  File "/usr/local/lib/python3.6/site-packages/graph_tool/draw/cairo_draw.py", line 1130, in graph_draw
-    output_size[1])
-SystemError: <class 'cairo.ImageSurface'> returned NULL without setting an error
+ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
 
 '''
 
 '''
-g.set_working_graph('big_data_supercats')
+g.set_working_graph('main')
+SubGraph(g).create(title='Ethereum_article_1_super_gt', seed=[41754003], cats=True,
+                   subcats=0, supercats=1)
+g.set_working_graph('Ethereum_article_1_super_gt')
+SubGraphProcessor(g).create_gt_subgraph()
+Snapshots(g).create('snapshot_year')
+SubGraphProcessor(g).internalize_snapshots('snapshot_year')
+#RTL(g).snapshots('snapshot_year', seed=28249265, vsize='cscore', vlabel='title', esize='cscore')
 SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-FR(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
+ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
 
-g.set_working_graph('big_data_subcats')
+g.set_working_graph('main')
+SubGraph(g).create(title='Ethereum_article_2_super_gt', seed=[41754003], cats=True,
+                   subcats=0, supercats=2)
+g.set_working_graph('Ethereum_article_2_super_gt')
+SubGraphProcessor(g).create_gt_subgraph()
+Snapshots(g).create('snapshot_year')
+SubGraphProcessor(g).internalize_snapshots('snapshot_year')
+#RTL(g).snapshots('snapshot_year', seed=28249265, vsize='cscore', vlabel='title', esize='cscore')
 SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-FR(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
+ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
 
-g.set_working_graph('prediction_subcats')
-#RTL(g).snapshots('snapshot_year', seed=1436435, vsize='cscore', vlabel='title', esize='cscore')
+g.set_working_graph('main')
+SubGraph(g).create(title='Blockchain_article_1_super_gt', seed=[44065971], cats=True,
+                   subcats=0, supercats=1)
+g.set_working_graph('Blockchain_article_1_super_gt')
+SubGraphProcessor(g).create_gt_subgraph()
+Snapshots(g).create('snapshot_year')
+SubGraphProcessor(g).internalize_snapshots('snapshot_year')
+#RTL(g).snapshots('snapshot_year', seed=28249265, vsize='cscore', vlabel='title', esize='cscore')
 SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-FR(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
+ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
 
-g.set_working_graph('prediction_supercats')
+g.set_working_graph('main')
+SubGraph(g).create(title='Blockchain_article_2_super_gt', seed=[44065971], cats=True,
+                   subcats=0, supercats=2)
+g.set_working_graph('Blockchain_article_2_super_gt')
+SubGraphProcessor(g).create_gt_subgraph()
+Snapshots(g).create('snapshot_year')
+SubGraphProcessor(g).internalize_snapshots('snapshot_year')
+#RTL(g).snapshots('snapshot_year', seed=28249265, vsize='cscore', vlabel='title', esize='cscore')
 SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-FR(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-
-g.set_working_graph('omics_subcats')
-#RTL(g).snapshots('snapshot_year', seed=13693058, vsize='cscore', vlabel='title', esize='cscore')
-SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-FR(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-
-g.set_working_graph('omics_supercats')
-SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-FR(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
+ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
 '''
-
-#g.set_working_graph('main')
-#RTL(g).snapshots('snapshot_year', seed=40423498, vsize='cscore', vlabel='title', esize='cscore')
-#ARF(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-#SFDP(g).snapshots('snapshot_year', vsize='cscore', vlabel='title', esize='cscore')
-#FR(g).snapshots('snapshot_year', vsize='cscore', vlabel='title')
-
-
-
-
-#g.set_working_graph('main')
-#SubGraphViews(g).create('big_data_subcats', 'snapshot_year',  seed=[40423498], cats=True,
-#                        subcats=5, supercats=0)
-
-#g.set_working_graph('main')
-#SubGraphViews(g).create('big_data_supercats', 'snapshot_year',  seed=[40423498], cats=True,
-#                        subcats=0, supercats=5)
-
-
-
-pp = pprint.PrettyPrinter(indent=4)
-
-#g = mp.gt_graph_objs['fixed_none__errors_removed']
-#g.set_working_graph('main')
-#SubGraphViews(g).create('bitcoin_subcats', 'snapshot_year',  seed=[39563179], cats=True,
-#                        subcats=5, supercats=0)
-
-#SubGraphViews(g).create('bitcoin_supercats', 'snapshot_year',  seed=[39563179], cats=True,
-#                        subcats=0, supercats=5)
-
-
-
-
-
-
-
-#GtEvents(g).create('gt_events_all')
-#Snapshots(g).create('snapshot_year'
-## from wikiCat.selectors.SelectorCscore import SelectorCscore
-#SelectorCscore(g).get_highest_cscores('nodes', cats_only=True, n=10)
-#mp.find_start_date()
-#mp.set_dump_date('2016-11-01')
-#mp.find_start_date()
-#mp.set_dump_date('2016-11-01')
-#pp.pprint(mp.pinfo)
-#WikiGraph(mp).
-#GtGraphGenerator(mp).register_gt_graph()
-
-#GENERATE SUBGRAPH AND VIEW
-#SubGraphViews(g).create('bitcoin_subcats', 'snapshot_year',  seed=[39563179], cats=True,
-#                        subcats=10, supercats=0)
-
-#SubGraph(g).create(title='bitcoin', seed=[40261770, 39563179, 42031444, 42132341], subcats=3, supercats=3)
-
-
-
-
-
-#Selector(g).create_snapshot_views_spark()
-
-#Selector(mp).temporal_views_spark()
-
-#ParsedDataErrorCheck(mp, 'cat_data').missing_info_source_ids()
-#ParsedDataErrorCheck(mp, 'cat_data').find_unresolvable_target_titles()
-
-#mp.find_oldest_revision()
-
-
-
-#GraphDataGenerator(mp, 'cat_data').generate_graph_data(override=True)
-#Generate graph
-#GtGraphGenerator(mp).create_gt_graph()
-#ControvercyScore(mp).calculate_edge_score()
-#ControvercyScore(mp).calculate_avg_node_score()
-#ControvercyScore(mp).calculate_avg_edge_score()
-
-#GtGraphGenerator(mp).add_gt_graph()
-
-
-
-#cat_data_fixed_none_errors_removed_1_edge
-
-#cs = ControvercyScore(mp)
-#cs.calculate_edge_score()
-#cs.caculate_node_score()
-
-
-#pp.pprint(mp.pinfo)
 
 
 '''
