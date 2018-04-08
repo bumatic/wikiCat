d6c9496ef711afd221119adc2eb9b62e6b449fcb
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2018-04-08 18:30:48 +0200

    processing steps changed

--- 03_generate_graph_data.py
+++ 03_generate_graph_data.py
@@ -1,21 +1,4 @@
-from datetime import datetime
 from wikiCat.wikiproject import Project
 
-#mp = Project(init=True)
-#mp.set_title = 'WikiCat'
-#mp.set_dump_date = '2018-03-01'
-
-
 mp = Project()
-
-#mp.add_graph_data('test_nodes.csv', 'test_edges.csv', 'test_events.csv')
-#mp.find_start_date()
-#mp.calculate_cscores()
 mp.generate_gt_graph()
-
-#mp.create_subgraph('testtest', [10010944])
-
-#end = datetime.now()
-#duration = end-start
-#print('Duration:')
-#print(str(duration.min) +':'+str(duration.seconds))
