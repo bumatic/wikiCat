adfe534fae22980d2acc8f00e50ef5473d2e909f
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2018-04-09 09:37:05 +0200

    cscore calc

---
+++ 02b_calc_cscores.py
@@ -0,0 +1,9 @@
+from wikiCat.wikiproject import Project
+
+mp = Project()
+mp.calculate_cscores()
+'''
+mp.add_parsed_data(page_info='enwiki-2018-03-01-page-info.csv',
+                   revision_info='enwiki-2018-03-01-revisions.csv',
+                   cat_data='enwiki-2018-03-01-cats.csv')
+'''
