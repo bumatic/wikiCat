e14570848cac014e2c1b13822a1627177dc3133c
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2018-04-11 22:17:13 +0200

    cscore

--- wikiCat/processor/pandas_processor.py
+++ wikiCat/processor/pandas_processor.py
@@ -9,30 +9,65 @@ class PandasProcessor(Processor):
 
     def load_events(self, file, cscore=True):
         if cscore:
+            dtype = {
+                'revision': float,
+                'source': int,
+                'target': int,
+                'event': str,
+                'cscore': float
+            }
             events = pd.read_csv(file, header=None, delimiter='\t',
-                                 names=['revision', 'source', 'target', 'event', 'cscore'], na_filter=False)
+                                 names=['revision', 'source', 'target', 'event', 'cscore'], dtype=dtype, na_filter=False)
         else:
+            dtype = {
+                'revision': float,
+                'source': int,
+                'target': int,
+                'event': str
+            }
             events = pd.read_csv(file, header=None, delimiter='\t',
-                                 names=['revision', 'source', 'target', 'event'], na_filter=False)
+                                 names=['revision', 'source', 'target', 'event'], dtype=dtype, na_filter=False)
         return events
 
     def load_edges(self, file, cscore=True):
         if cscore:
+            dtype = {
+                'source': int,
+                'target': int,
+                'type': str,
+                'cscore': float
+            }
             edges = pd.read_csv(file, header=None, delimiter='\t',
-                                names=['source', 'target', 'type', 'cscore'], na_filter=False)
+                                names=['source', 'target', 'type', 'cscore'], dtype=dtype, na_filter=False)
         else:
-                edges = pd.read_csv(file, header=None, delimiter='\t',
-                                    names=['source', 'target', 'type'], na_filter=False)
+            dtype = {
+                'source': int,
+                'target': int,
+                'type': str
+            }
+            edges = pd.read_csv(file, header=None, delimiter='\t',
+                                names=['source', 'target', 'type'], dtype=dtype, na_filter=False)
         return edges
 
     def load_nodes(self, file, cscore=True):
         # Default node columns ['id', 'title', 'ns', ('cscore')]
         if cscore:
+            dtype = {
+                'id': int,
+                'title': str,
+                'ns': int,
+                'cscore': float
+            }
             nodes = pd.read_csv(file, header=None, delimiter='\t',
-                                names=['id', 'title', 'ns', 'cscore'], na_filter=False)
+                                names=['id', 'title', 'ns', 'cscore'], dtype=dtype, na_filter=False)
         else:
+            dtype = {
+                'id': int,
+                'title': str,
+                'ns': int
+            }
             nodes = pd.read_csv(file, header=None, delimiter='\t',
-                                names=['id', 'title', 'ns', 'cscore'], na_filter=False)
+                                names=['id', 'title', 'ns'], dtype=dtype, na_filter=False)
         return nodes
 
     def highest_cscores(self, df, n=100, save=False, outfile=None):
