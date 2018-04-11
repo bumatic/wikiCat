61beb882c88ce75a79c6ae78a07d24bc86252fd5
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2018-04-11 16:25:17 +0200

    debugging

--- debugging.py
+++ debugging.py
@@ -2,15 +2,15 @@
 import pandas as pd
 
 path = 'project/02_gt_graphs/contents_3sub/contents_3sub_cats_1_edges.csv'
-
+print(path)
 dtype = {
     'source': int,
     'target': int,
     'type': str,
-    'cscore': float
+    'cscore': np.float64
         }
 
-df =pd.read_csv(path, header=None, delimiter='\t', names=['source', 'target', 'type', 'cscore'],
-                dtype=dtype, na_filter=False)
+df = pd.read_csv(path, header=None, delimiter='\t', names=['source', 'target', 'type', 'cscore'],
+                 dtype=dtype, na_filter=False)
 df = df[['source', 'target']]
 print(df)
