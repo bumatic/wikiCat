61499edfd90d45f76de4b5ba87af2947b2d792bb
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2018-04-12 07:32:43 +0200

    processing

---
+++ 01 init project.py
@@ -0,0 +1,5 @@
+from wikiCat.wikiproject import Project
+
+mp = Project(init=True)
+mp.set_title('WikiCat')
+mp.set_dump_date('2018-03-01')
---
+++ 03_generate_graph_data.py
@@ -0,0 +1,4 @@
+from wikiCat.wikiproject import Project
+
+mp = Project()
+mp.generate_gt_graph()
