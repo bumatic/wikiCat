0c6fdf4ead57f9a94c8d6a8f5ea5f584af6d1ba7
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2018-04-14 15:14:42 +0200

    processing

--- c_generate_gt.py
+++ c_generate_gt.py
@@ -1,4 +1,15 @@
 from wikiCat.wikiproject import Project
 
 mp = Project()
+
+mp.remove_subgraph('main')
 mp.generate_gt_graph()
+
+'''
+mp.graph.set_working_graph(key='main')
+mp.create_snapshots(title='snapshots_month', slice='month')
+mp.graph.set_working_graph(key='main')
+mp.create_snapshots(title='snapshots_year', slice='year')
+mp.graph.set_working_graph(key='main')
+mp.create_snapshots(title='first_year_snapshots_day', slice='day', start_date='2003-01-05', end_date='2004-01-05')
+'''
