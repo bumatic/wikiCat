7379f920edfaf9b66f4ad4d7212fb12770a4b0c8
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2018-04-16 00:34:57 +0200

    processing

---
+++ e_create_subgraph.py
@@ -0,0 +1,11 @@
+from wikiCat.wikiproject import Project
+
+mp = Project()
+
+# Contents 1sub 1super
+
+mp.graph.set_working_graph(key='main')
+mp.create_subgraph(title='contents_2sub', seed=[14105005], cats=True,
+                   subcats=1, supercats=0)
+mp.create_snapshots(graph='contents_2sub', title='snapshots_year')
+#mp.create_static_viz('contents_2sub', 'SFDP', snapshots='snapshots_year', drawing_props_file='static.json')
