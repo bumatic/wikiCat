6f9dbb222e9ba06398bc7d3829c95814c3a34d4c
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2018-04-16 00:51:43 +0200

    processing

--- e_create_subgraph.py
+++ e_create_subgraph.py
@@ -6,6 +6,6 @@ mp = Project()
 
 mp.graph.set_working_graph(key='main')
 mp.create_subgraph(title='contents_2sub', seed=[14105005], cats=True,
-                   subcats=1, supercats=0)
+                   subcats=2, supercats=0)
 mp.create_snapshots(graph='contents_2sub', title='snapshots_year')
 #mp.create_static_viz('contents_2sub', 'SFDP', snapshots='snapshots_year', drawing_props_file='static.json')
--- wikiCat/processor/python.py
+++ wikiCat/processor/python.py
@@ -2,5 +2,6 @@ from wikiCat.wikiproject import Project
 from wikiCat.processor.gt_sub_graph_processor import SubGraphProcessor
 
 mp = Project()
+mp.remove_subgraph('contents_2sub')
 title = 'snapshots_year'
 SubGraphProcessor(mp).internalize_snapshots(title)
