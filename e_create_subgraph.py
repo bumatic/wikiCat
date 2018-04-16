3aa9befaeb9c7f9197253b364f364a5208d64c16
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2018-04-16 18:32:44 +0200

    processing

--- e_create_subgraph.py
+++ e_create_subgraph.py
@@ -1,26 +1,10 @@
 from wikiCat.wikiproject import Project
 
 mp = Project()
-mp.create_snapshots(graph='main', title='snapshots_month_2004_2005', slice='month', end_date='2005-12-31')
 
-
+mp.remove_subgraph('bitcoin_2super')
 mp.graph.set_working_graph(key='main')
-mp.create_subgraph(title='blockchains_cat_1sub_1super', seed=[48210587], cats=True,
-                   subcats=1, supercats=1)
-mp.create_snapshots(graph='blockchains_cat_1sub_1super', title='snapshots_year')
-
-mp.graph.set_working_graph(key='main')
-mp.create_subgraph(title='blockchain_2super', seed=[44065971], cats=True,
-                   subcats=0, supercats=2)
-mp.create_snapshots(graph='blockchain_2super', title='snapshots_year')
-
-mp.graph.set_working_graph(key='main')
-mp.create_subgraph(title='bitcoin_blockchain_ethereum_2super', seed=[28249265, 44065971, 41754003], cats=True,
-                   subcats=0, supercats=2)
-mp.create_snapshots(graph='bitcoin_blockchain_ethereum_2super', title='snapshots_year')
-
-mp.graph.set_working_graph(key='main')
-mp.create_subgraph(title='bitcoin_2super', seed=[2824926], cats=True,
+mp.create_subgraph(title='bitcoin_2super', seed=[28249265], cats=True,
                    subcats=0, supercats=2)
 mp.create_snapshots(graph='bitcoin_2super', title='snapshots_year')
 
