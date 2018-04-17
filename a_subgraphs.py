2069a940686efcd1e72e17fc697ffcd3310d4ea0
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2018-04-17 09:08:26 +0200

    processing

--- a_subgraphs.py
+++ a_subgraphs.py
@@ -1,6 +1,7 @@
 from wikiCat.wikiproject import Project
 mp = Project()
 
+'''
 mp.graph.set_working_graph(key='main')
 mp.create_subgraph(title='history_of_computing_1super', seed=[1506937], cats=True,
                    subcats=0, supercats=1)
@@ -22,5 +23,6 @@ mp.create_static_viz('algorithms_1sub_1super', 'SFDP', snapshots='snapshots_year
 mp.graph.set_working_graph(key='main')
 mp.create_subgraph(title='artificial_intelligence_1sub_1super', seed=[700355], cats=True,
                    subcats=1, supercats=1)
-mp.create_snapshots(graph='artificial_intelligence_2sub_1super', title='snapshots_year')
+'''
+mp.create_snapshots(graph='artificial_intelligence_1sub_1super', title='snapshots_year')
 mp.create_static_viz('artificial_intelligence_1sub_1super', 'SFDP', snapshots='snapshots_year', drawing_props_file='static.json')
