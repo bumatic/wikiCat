1173d4c42c3a0f5e3a3835e2e62430d58c018d82
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2018-04-17 07:38:35 +0200

    processing

--- e_create_subgraph.py
+++ e_create_subgraph.py
@@ -1,8 +1,6 @@
 from wikiCat.wikiproject import Project
 mp = Project()
 
-
-mp.create_static_viz('main', 'SFDP', snapshots='snapshots_month_2004_2005', drawing_props_file='static.json')
 mp.create_static_viz('contents_1sub', 'SFDP', snapshots='snapshots_year', drawing_props_file='static.json')
 mp.create_static_viz('bitcoin_2super', 'SFDP', snapshots='snapshots_year', drawing_props_file='static.json')
 mp.create_static_viz('bitcoin_blockchain_ethereum_2super', 'SFDP', snapshots='snapshots_year', drawing_props_file='static.json')
@@ -11,7 +9,7 @@ mp.create_static_viz('blockchain_2super', 'SFDP', snapshots='snapshots_year', dr
 mp.create_static_viz('brexit_2super', 'SFDP', snapshots='snapshots_year', drawing_props_file='static.json')
 mp.create_static_viz('contents_1super', 'SFDP', snapshots='snapshots_year', drawing_props_file='static.json')
 mp.create_static_viz('contents_2sub', 'SFDP', snapshots='snapshots_year', drawing_props_file='static.json')
-
+mp.create_static_viz('main', 'SFDP', snapshots='snapshots_month_2004_2005', drawing_props_file='static.json')
 
 
 
