313df4aae693be0c9d05e9e0844046c3d6046bd5
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2018-09-16 10:02:32 +0200

    author processing test

--- examples/xm_generate_graph_data.py
+++
@@ -1,9 +0,0 @@
-from wikiCat.wikiproject import Project
-
-mp = Project(project_path='test_project')
-
-#mp.generate_graph_data()
-mp.find_start_date()
-mp.set_dump_date('2018-03-01')
-#mp.create_subgraph(title='test', seed=[12], subcats=5)
-#mp.create_snapshots(graph='test', start_date='2004-06-02')
