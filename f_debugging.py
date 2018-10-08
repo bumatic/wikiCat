320c92a9f383dca39f1e5ce3584c30aed0db98e0
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2018-10-08 09:29:49 +0200

    debugging

--- f_debugging.py
+++ f_debugging.py
@@ -10,17 +10,18 @@ from wikiCat.processor.spark_processor_graph import SparkProcessorGraph
 from dateutil import parser
 
 
-
 def mapper_revisions(line):
     fields = line.split('\t')
     rev_id = int(fields[1])
     rev_date = parser.parse(fields[2])
     rev_date = rev_date.timestamp()
+    counter = 0
     try:
         rev_author = int(float(fields[3]))
     except:
         rev_author = 0
-        print(fields[3])
+        counter+=1
+        print(fields)
     return Row(rev_id=rev_id, rev_date=rev_date, rev_author=rev_author)
 
 spark = SparkSession\
