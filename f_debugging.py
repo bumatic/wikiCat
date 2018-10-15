1e7d2c0fd7fe0832beedebb7f6d0f9a9907ae49d
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2018-10-15 23:29:25 +0200

    Update f_debugging.py

--- f_debugging.py
+++ f_debugging.py
@@ -35,17 +35,64 @@ spark = SparkSession\
 print(SparkConf().getAll())
 
 source = spark.sparkContext.textFile(os.path.join('project', '01_data', '01_parsed', 'revisions.csv'))
+
+'''
+# check which file is read in for processing
+
 print(source)
 
-#tmp_results_file = os.path.join('project', '01_data', '01_parsed', 'tmp_results.csv')
+# Result: project/01_data/01_parsed/revisions.csv MapPartitionsRDD[1] at textFile at NativeMethodAccessorImpl.java:0
+'''
+
+# tmp_results_file = os.path.join('project', '01_data', '01_parsed', 'tmp_results.csv')
 
 events = source.map(mapper_revisions)
 events_df = spark.createDataFrame(events).cache()
 events_df.createOrReplaceTempView("events")
+
+'''
+# check structure in revisions.csv
+
 events_df.show()
 
-# print(events_df.count())
+# Results:
+# +----------+-------------+-------+
+# |rev_author|     rev_date| rev_id|
+# +----------+-------------+-------+
+# |         0|1.006436661E9| 275670|
+# |   8083618| 1.00651066E9| 275671|
+# |         0|1.014651791E9| 172562|
+# |         0|1.030213496E9| 172568|
+# |         0|1.030213737E9| 423064|
+# |       198|1.044798028E9| 712878|
+# |      4444|1.052601796E9| 992695|
+# |     10121|1.059464905E9|1231098|
+# |     21737|1.063855724E9|1456275|
+# |     13833|1.064047087E9|1463368|
+# |      1926|1.064170102E9|1565240|
+# |     10059|1.067589017E9|1781865|
+# |     10059|1.069388259E9|1972324|
+# |     21737|1.071723655E9|2124588|
+# |      9475|1.073739349E9|2635411|
+# |     21737|1.079577047E9|2812990|
+# |     21737|1.079577063E9|2813083|
+# |     21737|1.079577594E9|2989587|
+# |     24180|1.080703494E9|3106710|
+# |     13833|1.081401434E9|3525021|
+# +----------+-------------+-------+
+# only showing top 20 rows
+'''
+
+'''
+# Check number of rows in revisions.csv
+
+print(events_df.count())
+
 # Result: 213087340
+'''
+
+
+# Filter revisions for specific revision date in order to check for inconsistencies
 
 events_filtered_df = events_df.filter(events_df.rev_date == 1.390047722E9) #1.030213496E9
 events_filtered_df.show()
