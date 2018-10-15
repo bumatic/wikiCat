9efae6680919d62d7bb1ceaeefb9f25c7ae847d2
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2018-10-15 23:18:58 +0200

    Update f_debugging.py

--- f_debugging.py
+++ f_debugging.py
@@ -43,7 +43,10 @@ events = source.map(mapper_revisions)
 events_df = spark.createDataFrame(events).cache()
 events_df.createOrReplaceTempView("events")
 events_df.show()
-print(events_df.count())
+
+# print(events_df.count())
+# Result: 213087340
+
 events_filtered_df = events_df.filter(events_df.rev_date == 1.390047722E9) #1.030213496E9
 events_filtered_df.show()
 
