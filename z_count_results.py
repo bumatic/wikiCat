8c2378d0b317de668b22c71883c6be0237cf07c6
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2018-10-17 08:22:59 +0200

    Update z_count_results.py

--- z_count_results.py
+++ z_count_results.py
@@ -6,10 +6,10 @@ directory = os.path.join('project', '01_data', '02_data_graph')
 files = ['events.csv', 'edges.csv', 'nodes.csv']
 
 
-for file in tqdm(files):
+for file in files:
     source = os.path.join(directory, file)
     chunksize = 1000000
     counter = 0
-    for chunk in tqdm(pd.read_csv(source, file, header=None, delimiter='\t', na_filter=False, chunksize=chunksize)):
+    for chunk in pd.read_csv(source, file, header=None, delimiter='\t', na_filter=False, chunksize=chunksize):
         counter = counter + chunk.shape[0]
     print('Number of rows in ' + file + ': ' + str(counter))
