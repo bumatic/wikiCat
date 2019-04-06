3dfdf4f055433c69d91c3a75ed38b7b6bda46012
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2019-04-06 19:14:13 +0200

    Update DEBUG.py

--- DEBUG.py
+++ DEBUG.py
@@ -101,7 +101,7 @@ revision_info = revision_info_source.map(mapper_revisions)
 revision_info_df = spark.createDataFrame(revision_info).cache()
 revision_info_df.createOrReplaceTempView("revision")
 
-revision_info_df = spark.sql("SELECT * FROM revision WHERE rev_author = 76.0")
+revision_info_df = spark.sql("SELECT * FROM revision WHERE rev_author = 76.0 AND rev_date = 1016010777.0")
 revision_info_df.show()
 revision_info_df.createOrReplaceTempView("revision")
 
