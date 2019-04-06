e031f72b04debcbfd0b2fc335454229884ca27d7
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2019-04-06 17:38:44 +0200

    Update DEBUG.py

--- DEBUG.py
+++ DEBUG.py
@@ -9,11 +9,18 @@ date = 1016010777.0
 #file = os.path.join('project', '01_data', '01_parsed', 'r.csv')
 #revs = pd.read_csv(file, header=None, sep='\t', names=['err', 'source', 'rev', 'date', 'author'])
 #revs = revs[['source', 'rev', 'date', 'author']]
+#date = parser.parse('2004-03-18T02:30:47Z').timestamp()
 
 # For regular data
 file = os.path.join('project', '01_data', '01_parsed', 'revisions.csv')
 revs = pd.read_csv(file, header=None, sep='\t', names=['source', 'rev', 'date', 'author'])
 
-
-revs['datetime'] = revs['date'].apply(lambda x: parser.parse(x).timestamp())
-print(revs[revs['datetime'] == date])
+chunksize = 1000000
+revisions = pd.DataFrame()
+for revs in pd.read_csv(file, header=None, sep='\t', names=['err', 'source', 'rev', 'date', 'author'], chunksize=chunksize):
+    revs['datetime'] = revs['date'].apply(lambda x: parser.parse(x).timestamp())
+    if len(revs[revs['datetime'] == date]) > 0:
+        print(revs[revs['datetime'] == date])
+        revisions = revisions.append(revs[revs['datetime'] == date])
+revisions.to_csv('DEBUG_revisions.csv', sep='\t', header=False, index=False)
+print(revisions)
