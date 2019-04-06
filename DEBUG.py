0d1b2a43268213e1cc073f27034043280eb25c90
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2019-04-06 17:43:33 +0200

    Update DEBUG.py

--- DEBUG.py
+++ DEBUG.py
@@ -2,25 +2,29 @@ import pandas as pd
 import os
 from dateutil import parser
 
-
-date = 1016010777.0
-
-# For Testdata
-#file = os.path.join('project', '01_data', '01_parsed', 'r.csv')
-#revs = pd.read_csv(file, header=None, sep='\t', names=['err', 'source', 'rev', 'date', 'author'])
-#revs = revs[['source', 'rev', 'date', 'author']]
-#date = parser.parse('2004-03-18T02:30:47Z').timestamp()
-
-# For regular data
+#  Debugging
 file = os.path.join('project', '01_data', '01_parsed', 'revisions.csv')
-revs = pd.read_csv(file, header=None, sep='\t', names=['source', 'rev', 'date', 'author'])
-
+date = 1016010777.0
+names = ['source', 'rev', 'date', 'author']
 chunksize = 1000000
+
+test = False
+
+# Debugging Test:
+if test:
+    file = os.path.join('project', '01_data', '01_parsed', 'r.csv')
+    date = parser.parse('2004-03-18T02:30:47Z').timestamp()
+    names = ['err', 'source', 'rev', 'date', 'author']
+    chunksize = 10
+
+
 revisions = pd.DataFrame()
-for revs in pd.read_csv(file, header=None, sep='\t', names=['err', 'source', 'rev', 'date', 'author'], chunksize=chunksize):
+for revs in pd.read_csv(file, header=None, sep='\t', names=names, chunksize=chunksize):
     revs['datetime'] = revs['date'].apply(lambda x: parser.parse(x).timestamp())
     if len(revs[revs['datetime'] == date]) > 0:
         print(revs[revs['datetime'] == date])
         revisions = revisions.append(revs[revs['datetime'] == date])
+    else:
+        print(False)
 revisions.to_csv('DEBUG_revisions.csv', sep='\t', header=False, index=False)
 print(revisions)
