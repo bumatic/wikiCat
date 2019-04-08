bd788e122ed5b7f4eb4b7510777474a273124af0
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2019-04-08 14:52:09 +0200

    Update DEBUG.py

--- DEBUG.py
+++ DEBUG.py
@@ -97,6 +97,7 @@ missing_author_row = spark.createDataFrame([[-1, "NO_AUTHOR_DATA"]])
 author_info_df = author_info_df.union(missing_author_row)
 author_info_df.createOrReplaceTempView("author")
 
+'''
 revision_info_source = spark.sparkContext.textFile(rev_file)
 revision_info = revision_info_source.map(mapper_revisions)
 revision_info_df = spark.createDataFrame(revision_info).cache()
@@ -106,12 +107,11 @@ revision_info_df = spark.sql("SELECT * FROM revision WHERE rev_author = 76.0 AND
 revision_info_df.show()
 revision_info_df.createOrReplaceTempView("revision")
 
-#resolved_authors_df = spark.sql('SELECT rev_id, rev_date, rev_author, author_id, author_name, '
-#                                'FROM revision r LEFT OUTER JOIN author a ON r.rev_author = a.author_id')
-
 resolved_authors_df = spark.sql(
             'SELECT r.rev_id, r.rev_date, a.author_name as rev_author, a.author_id as rev_author_id '
             'FROM revision r LEFT OUTER JOIN author a ON r.rev_author = a.author_id')
-
-
 resolved_authors_df.show()
+'''
+
+authors_reduced_df = spark.sql('SELECT * FROM author WHERE author_id = 76.0')
+authors_reduced_df.show()
