7f83ca044fe97462956603abca54df2cc27bff2d
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2019-04-08 11:36:31 +0200

    Update DEBUG.py

--- DEBUG.py
+++ DEBUG.py
@@ -106,6 +106,6 @@ revision_info_df = spark.sql("SELECT * FROM revision WHERE rev_author = 76.0 AND
 revision_info_df.show()
 revision_info_df.createOrReplaceTempView("revision")
 
-#resolved_authors_df = spark.sql('SELECT r.rev_id, r.rev_date, rev_author, a.author_name as rev_author_name '
-#                                'FROM revision r LEFT OUTER JOIN author a ON r.rev_author = a.author_id')
-#resolved_authors_df.show()
+resolved_authors_df = spark.sql('SELECT r.rev_id, r.rev_date, rev_author, a.author_name as rev_author_name '
+                                'FROM revision r LEFT OUTER JOIN author a ON r.rev_author = a.author_id')
+resolved_authors_df.show()
