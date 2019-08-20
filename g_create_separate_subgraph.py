2f599baa4f0d1fc8f41145e823cc597a0689edf8
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2019-08-20 23:08:53 +0200

    Update g_create_separate_subgraph.py

--- g_create_separate_subgraph.py
+++ g_create_separate_subgraph.py
@@ -2,7 +2,7 @@ from wikiCat.wikiproject import Project
 
 mp = Project()
 
-mp.create_separate_subgraph(title='ict', seed=['12'],
+mp.create_separate_subgraph(title='ict', seed=['50886144'],
                             cats=True, subcats=10, supercats=1,
                             links=True, inlinks=1, outlinks=1)
 
