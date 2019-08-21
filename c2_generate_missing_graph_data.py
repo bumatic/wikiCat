41e0bc460643cab5bf82b3df966ab76ab280c64a
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2019-08-21 11:04:30 +0200

    Create c2_generate_missing_graph_data.py

---
+++ c2_generate_missing_graph_data.py
@@ -0,0 +1,31 @@
+from wikiCat.wikiproject import Project
+from wikiCat.processor.graph_data_gererator import GraphDataGenerator
+mp = Project()
+
+
+
+processing = {
+    "graph_data": {
+        "page_info": "done",
+        "cats": {},
+        "links": {
+            "enwiki-20180701-pages-meta-history10.xml-p2505803p2535938_links.csv.7z": "init",
+            "enwiki-20180701-pages-meta-history13.xml-p5096987p5137296_links.csv.7z": "init"
+        }
+    }
+}
+
+print(processing)
+
+processor = GraphDataGenerator()
+
+for k in processing['links'].keys():
+    if processing['links'][k] == 'init':
+        processor.generate_manual('links', k, resolve_authors=False)
+
+'''
+self.project.pinfo['processing']['graph_data']['links'][link] = \
+            self.generate(edge_type, link, resolve_authors=resolve_authors)
+'''
+
+
