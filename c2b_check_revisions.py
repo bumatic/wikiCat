f97275a930c7270b21f9752c2d0e876d289daea7
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2019-08-27 08:53:59 +0200

    Update c2b_check_revisions.py

--- c2b_check_revisions.py
+++ c2b_check_revisions.py
@@ -12,13 +12,14 @@ from tqdm import tqdm
 
 
 chunksize = 10000000
-rev_data_file = os.path.join('project', '01_data', '01_parsed', 'revisions.csv')
+#rev_data_file = os.path.join('project', '01_data', '01_parsed', 'revisions.csv')
+rev_data_file = os.path.join('manual_graph_data_generation', 'revisions_parsed_new.csv')
 rev_data = pd.read_csv(rev_data_file, delimiter='\t', names=['page_id', 'rev_id', 'ts', 'author_id'], chunksize=chunksize)
 
 for chunk in rev_data:
     #print (chunk.dtypes)
-    #print(event_data.merge(chunk, left_on='rev_id', right_on='rev_id'))
-    print(chunk[chunk['page_id'] == 2513427])
+    print(event_data.merge(chunk, left_on='rev_id', right_on='rev_id'))
+    #print(chunk[chunk['page_id'] == 2513427])
 
 
 '''
